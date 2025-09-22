75866f3458d806ede73f7a204410b1267a7b633f
hytkim <145666445+hytkim@users.noreply.github.com>
2025-09-22 15:01:45 +0900

    api
    게시판 글목록 조회기능 벌써완성해버림 ㄷㄷ api까지써서

--- public/js/8th/covid.js
+++ public/js/8th/covid.js
@@ -8,13 +8,15 @@ fetch(url)
   .then((result) => {
     // console.log(result);
     total = result.data;
-    showPageList(2);
+    showPageList(1);
     // console.log(result.data[0]);
   })
   .catch((err) => console.log(err));
 
 //페이지 목록 출력
 showPageList = (page = 1) => {
+  // 초기화
+  document.querySelector('#centerList').innerHTML = '';
   let start, end;
   // start = (--page * 10);
   start = (page-1) * 10;
@@ -24,6 +26,99 @@ showPageList = (page = 1) => {
   .forEach((center) => {
     let tr = makeRow(center);
     document.querySelector('#centerList').appendChild(tr);
+  });
+  makePagingList();
+};
+// 총 항목의 수에 따라 페이징 목록 => 13, 11,12,13,14,15,16,17,18,19 ... 29
+makePagingList = () => {
+  const paging = {
+    currPage:1,
+    startPage:1,
+    endPage:10,
+    prev:false,
+    next:false,
+    initPage(page = 1, totalCnt = 284){
+      let realEnd = Math.ceil(totalCnt/10);
+      this.currPage = page; // 13
+      this.endPage = Math.ceil(page/10) * 10;// 올림 함수 13/10  => ceil(1.3) => 2 
+      this.startPage = this.endPage - 9;
+      this.prev = this.startPage == 1 ? false : true; // 이전의 10페이지가 있는지
+      // this.next = this.end > realEnd ? realEnd: this.end;
+      this.next = this.endPage < realEnd ? true : false;
+    }
+  }
+  paging.initPage(7);
+  console.log(paging);
+  let target = document.querySelector('ul.pagination');
+  target.innerHTML = '';
+  // ul tag
+  if (paging.prev) {
+    let li = document.createElement('li');
+    li.className = `page-item`;
+
+    let a = document.createElement('a');
+    a.innerText = 'Previous';
+    a.className = ' page-link';
+    li.appendChild(a);
+    target.appendChild(li);
+  }
+  else{
+    let li = document.createElement('li');
+    li.className = `page-item disabled`;
+
+    let a = document.createElement('a');
+    a.innerText = 'Previous';
+    a.className = ' page-link';
+    li.appendChild(a);
+    target.appendChild(li);
+  }
+
+  // ul tag
+  for(let s = paging.startPage; s <= paging.endPage; s++){
+    let li = document.createElement('li');
+    li.className = `page-item`
+    let a = document.createElement('a');
+    a.innerText = s;
+    a.className = ' page-link';
+    a.setAttribute('href', '#');
+
+    li.appendChild(a);
+    target.appendChild(li);
+  }
+
+  if (paging.next) {
+    let li = document.createElement('li');
+    li.className = `page-item`;
+
+    let a = document.createElement('a');
+    a.innerText = 'Next';
+    a.className = ' page-link';
+    li.appendChild(a);
+    target.appendChild(li);
+  }
+  else{
+    let li = document.createElement('li');
+    li.className = `page-item disabled`;
+
+    let a = document.createElement('a');
+    a.innerText = 'Next';
+    a.className = ' page-link';
+    li.appendChild(a);
+    target.appendChild(li);
+  }
+  // paging.initPage(1);
+  pageLinkEvnet();
+}
+
+//화면의 a태그에 링크 생성
+pageLinkEvnet = () => {
+  document.querySelectorAll('a.page-link').forEach((item, index, ary) => {
+    // 이벤트 등록
+    item.addEventListener('click', (e) => {
+      e.preventDefault(); // 기본 기능 차단
+      let page = item.innerHTML;
+      showPageList(page);
+    });
   });
 };
 
@@ -39,3 +134,4 @@ makeRow = (center) => {
   }
   return tr;
 };
+pageLinkEvnet();
